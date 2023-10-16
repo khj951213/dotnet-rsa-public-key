609f6ef327c928147fe3ee3b4108854594e3a2c6
khj951213 <55070877+khj951213@users.noreply.github.com>
2023-10-16 14:27:43 +1100

    Complete RSA PUb

---
+++ RSAPublicKeyEncrypt/JK_RSA_PUB/RSAEnc.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace JK_RSA_PUB
+{
+    public class RSAEnc
+    {
+        private static RSACryptoServiceProvider csp = new RSACryptoServiceProvider(2048);
+        private RSAParameters _privateKey;
+        private RSAParameters _publicKey;
+
+        public RSAEnc()
+        {
+            _privateKey = csp.ExportParameters(true);
+            _publicKey = csp.ExportParameters(false);
+        }
+
+        public string GetPublicKey()
+        {
+            var writer = new StringWriter();
+            var xs = new XmlSerializer(typeof(RSAParameters));
+            xs.Serialize(writer, _publicKey);
+            return writer.ToString();
+        }
+
+        public string Encrypt(string plainText)
+        {
+            csp = new RSACryptoServiceProvider();
+            csp.ImportParameters(_publicKey);
+            var data = Encoding.Unicode.GetBytes(plainText);
+            var cypher = csp.Encrypt(data, false);
+            return Convert.ToBase64String(cypher);
+        }
+
+        public string Decrypt(string cypherText)
+        {
+            var dataByts = Convert.FromBase64String(cypherText);
+            csp.ImportParameters(_privateKey);
+            var plainText = csp.Decrypt(dataByts, false);
+            return Encoding.Unicode.GetString(plainText);
+        }
+    }
+}
---
+++ RSAPublicKeyEncrypt/RSAPublicKeyEncrypt/Program.cs
@@ -0,0 +1,17 @@
+﻿using JK_RSA_PUB;
+
+Console.WriteLine("RSA Encryption");
+
+var rsa = new RSAEnc();
+var cypher = string.Empty;
+var plainText = string.Empty;
+
+Console.WriteLine("Enter your text to encrypt");
+var text = Console.ReadLine();
+if (!string.IsNullOrEmpty(text))
+{
+    cypher = rsa.Encrypt(text);
+    Console.WriteLine("Encrypted Text: " +  cypher);
+    plainText = rsa.Decrypt(cypher);
+    Console.WriteLine("Decrypted Text: " + plainText);
+}
